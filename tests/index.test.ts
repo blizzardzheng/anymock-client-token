ff303bc1c66dd8b345111101974be4b584549457
candy.zheng <caifeng.zxq@antfin.com>
2019-04-23 02:57:06 +0800

    fix:request

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,7 +3,7 @@ import 'mocha';
 
 describe('获取案例', async function () {
     it('用例1', async function () {
-      const token = await Helper('cff1558100f8864198cf119b779bbb90', {
+      const token = await Helper('6ee18138b57851686f03a7c598711031', {
         "type":"jsbridge",
         "subType": "RPC",
         "matching":"dfdfg",
