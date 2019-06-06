e5802b2887f9ab1c8cb7fdec3d6912085a6a35d7
candy.zheng <caifeng.zxq@antfin.com>
2019-06-06 14:55:23 +0800

    fix

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,13 +3,12 @@ import 'mocha';
 
 describe('获取案例', async function () {
     it('用例1', async function () {
-      const token = await Helper('6ee18138b57851686f03a7c598711031', {
-        "type":"jsbridge",
-        "subType": "RPC",
-        "matching":"dfdfg",
-        "pid":"582b0cf0",
-        "sid": "c1bd2670"
-      });
+      const token = await Helper('6ee18138b57851686f03a7c598711031', { 
+      type: 'jsbridge',
+      subType: 'httprequest',
+      matching: '/counterAdd',
+      pid: '3846d050' 
+    });
       console.log('token', token);
     });
 })
